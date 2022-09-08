1a02d009358a3b52ece46bcc8d0f0ecad64fe158
root <lidiyagez@gmail.com>
2022-09-08 07:30:31 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-root@3363de16e00c:/alx-low_level_programming/0x00-hello_world# cat >  4-puts.c
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - entrypoint
  *
- * Return: Always 0 (Success)
+ * Return: 0 after printing the function
  */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
