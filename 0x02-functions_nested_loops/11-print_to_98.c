f71395dbdf0c3954afd9248f78344ea10c1fb79a
root <lidiyagez@gmail.com>
2022-09-13 08:02:05 -0700

    tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,18 +8,18 @@
 */
 void print_to_98(int n)
 {
-        while (n != 98)
-        {
-                printf("%d, ", n);
-                if (n < 98)
-                {
-                        n++;
-                }
-                else
-                {
-                        n--;
-                }
-        }
-        printf("98");
-        printf("\n");
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n < 98)
+		{
+			n++;
+		}
+		else
+		{
+			n--;
+		}
+	}
+	printf("98");
+	printf("\n");
 }
