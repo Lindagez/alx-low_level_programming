89eaaf2d30d770f456f6bc8eb5cf5c70acd3a6c2
root <lidiyagez@gmail.com>
2022-09-13 07:04:03 -0700

    funs nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,9 +7,9 @@
  */
 void print_alphabet(void)
 {
-	char i;
+        char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
+        for (i = 'a'; i <= 'z'; i++)
+                _putchar(i);
+        _putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 - print alphabet [a..z]
+ * main - Entry point
  *
- * Return: - always (void)
+ * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)
-{
-	char i;
-	int j;
 
-	for (j = 0; j < 10; j++)
-	{
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
-	}
+int main(void)
+{
+        int i = 1;
+        char name[8] = "_putchar";
+
+        for (i = 0; i < 8; i++)
+        {
+                _putchar(name[i]);
+        }
+        _putchar('\n');
+        return (0);
 }
