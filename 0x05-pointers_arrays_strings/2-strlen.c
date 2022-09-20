f961b04e89b84f2ccd45fbf9fa56467503349744
root <lidiyagez@gmail.com>
2022-09-20 10:04:03 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,19 @@
 #include "main.h"
+
 /**
- * swap_int - a function that swaps the values of two integers
- * @a: input 1
- * @b: input 2
- * Return: integers
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
  */
-void swap_int(int *a, int *b)
+int _strlen(char *s)
 {
-	int n = *a;
-	*a = *b;
-	*b = n;
+	int i = 1, sum = 0;
+	char pl = s[0];
+
+	while (pl != '\0')
+	{
+		sum++;
+		pl = s[i++];
+	}
+	return (sum);
 }
