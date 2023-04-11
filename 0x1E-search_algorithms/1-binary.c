4eefb537b64c0f86737cc2a4a249e683aa4c788d
@lindagez <lidiyagez@gmail.com>
2023-04-11 11:06:54 +0300

    task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,36 +8,41 @@
  * @array: the array to display
 */
 
-void search_result(int *array, int low, int high)
+void search_result(int *array, int l, int r)
 {
-	printf("Searching  array: ");
-	for (;low < high; low++)
+	printf("Searching in array: ");
+	for (;l < r; l++)
 		printf("%i, ", array[l]);
-	printf("%i\n", array[high]);
+	printf("%i\n", array[r]);
 }
 
 /**
- * binary_search - search value in the given array using binary search***/
+ * binary_search - search value in the given array using binary algo
+ * @array: array contain numbers
+ * @size: the size of the array
+ * @value: the value to search
+ *
+ * Return: index if found, -1 if sth went wrong
+*/
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1;
-	int mid;
+	size_t l = 0, r = size - 1;
+	int m;
 
 	if (!array || !size)
 		return (-1);
 
-	while (low <= high)
+	while (l <= r)
 	{
 		search_result(array, l, r);
-		mid = (high + l) / 2;
-		if (array[mid] < value)
-			low = mid + 1;
-		else if (array[mid] > value)
-			high = mid - 1;
+		m = (r + l) / 2;
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
 		else
-			return (mid);
+			return (m);
 	}
 	return (-1);
 }
-
