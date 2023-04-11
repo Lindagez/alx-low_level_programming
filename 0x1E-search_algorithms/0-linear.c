af789da671bec27119ee8885816f3047858cd477
@lindagez <lidiyagez@gmail.com>
2023-04-11 10:24:19 +0300

    task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "search_algos.h"
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always EXIT_SUCCESS
+ *     */
 int linear_search(int *array, size_t size, int value) {
     
     for (int i = 0; i < size; i++) {
